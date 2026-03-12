e88b29bc5cba93336442bc76d868626bee77d4c1
shivgonare <105354810+shivgonare@users.noreply.github.com>
2026-03-12 10:49:32 +0530

    addFeature (#2)
    * added one line on feature branch
    
    * added demo2 line

--- src/main/java/com/apachebeam/main/PipelineApplication.java
+++ src/main/java/com/apachebeam/main/PipelineApplication.java
@@ -33,6 +33,8 @@ public class PipelineApplication {
 
         System.out.println("We are in the feature branch ");
 
+        System.out.println("We are in the feature branch  demo2  ");
+
 
         Pipeline pipeline = Pipeline.create(options);
 
